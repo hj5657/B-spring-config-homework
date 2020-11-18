e414cb2037b045e524bea86ee78f233cb0914e22
hejie <565703319@qq.com>
2020-11-18 15:36:39 +0800

    [hj] override use spring boot test properties

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+@SpringBootTest(properties = {"levelNumber=2"})
+@AutoConfigureMockMvc
+class LevelControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void should_return_basic_when_level_number_more_than_one() throws Exception {
+        mockMvc.perform(get("/level"))
+                .andExpect(jsonPath("$",is("advanced")));
+    }
+}
